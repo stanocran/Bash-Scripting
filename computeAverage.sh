c28779e3d0d3482ca49bded2e1d1f0c35333ca06
stanocran <stanocran@gmail.com>
2022-05-20 18:21:44 +0000

    Added constraints and descriptive output

--- computeAverage.sh
+++ computeAverage.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
+echo Enter Number of Integers
 read N
 O=0
 P=0
+if [ "$N" -gt 500 ]
+then 
+echo Invalid input
+exit 1
+
+elif [ "$N" -lt 1 ]
+then
+echo Invalid input
+exit 1
+fi
+
+echo 'Enter Integers for Average Calculation'
 #while loop used to implent number of integers taken as input
 while [ $P -lt $N ]; do
     read A
+if [ "$A" -gt 10000 ] 
+then 
+echo Invalid input
+exit 1
+
+elif  [ "$A" -lt -10000 ]
+then
+echo Invalid input
+exit 1
+fi
     O=$(($O+$A))
     P=$(($P+1))
 done
@@ -12,3 +35,6 @@ done
 echo $O / $N
  M=$(($O / $N)) 
 printf "%.3f\n" $M
+
+#Alternative method of achieving .000 decimal position
+#M=`echo "$sum/$num" | bc`

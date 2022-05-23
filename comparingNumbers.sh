d629f1038d7df9e7839fa117b852b92857ae391a
stanocran <stanocran@gmail.com>
2022-05-23 14:42:13 +0000

    Adding Number Comparison Script

--- comparingNumbers.sh
+++ comparingNumbers.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
+echo "Numbers to compare"
 read x 
 read y 
-#validate if input is an integer
-[[ "$x","$y" =~ ^?[0-9]+ ]] && echo error 
-exit 1
-
 
 [[ $x -gt $y ]] && echo 'X is greater than Y'
 [[ $x -eq $y ]] && echo 'X is equal to Y'
